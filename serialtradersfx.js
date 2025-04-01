b045c004a0939d753dfd2911696d1d01cc24124a
niwahasa <122720975+niwahasa@users.noreply.github.com>
2025-04-01 10:55:09 +0300

    Create serialtradersfx.js

---
+++ serialtradersfx.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("SerialTradersFX website loaded successfully!");
+});
+
+function learnMore() {
+    alert("Join us to elevate your trading skills and master forex strategies!");
+}
